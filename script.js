dcc6afd257b3b791ff434c22ec3b8629e0e36019
Nupur2401 <111990090+Nupur2401@users.noreply.github.com>
2026-03-14 20:45:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ async function loadChapters(subject) {
     document.getElementById("chapterInfo").textContent = "Fetching chapters…";
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=YOUR_API_KEY",
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyBWCBQUoSfLiWscBO8_LynVyVLPpgv-9nM",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
